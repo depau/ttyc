ea3e3fe3c1c27e722db8f3020880d78d7712f956
Davide Depau <davide@depau.eu>
2021-03-14 16:58:49 +0100

    Re-comment Trace

--- debug.go
+++ debug.go
@@ -1,15 +1,10 @@
 package ttyc
 
-import (
-	"fmt"
-	"runtime"
-)
-
 // https://stackoverflow.com/a/46289376/1124621
 func Trace() {
-	pc := make([]uintptr, 15)
-	n := runtime.Callers(2, pc)
-	frames := runtime.CallersFrames(pc[:n])
-	frame, _ := frames.Next()
-	fmt.Printf("%s:%d %s\n", frame.File, frame.Line, frame.Function)
+	//pc := make([]uintptr, 15)
+	//n := runtime.Callers(2, pc)
+	//frames := runtime.CallersFrames(pc[:n])
+	//frame, _ := frames.Next()
+	//fmt.Printf("%s:%d %s\n", frame.File, frame.Line, frame.Function)
 }
